40b53e7461dc4b6a535a16c0bd12b1f3c0a5e95f
nnilocoelho <nnilocoelho@gmail.com>
2021-09-15 15:24:50 -0300

    Introdução

---
+++ introducao/primeiro_programa.py
@@ -0,0 +1,10 @@
+# MEU PRIMEIRO PROGRAMA EM PYTHON
+a = 2
+b = 4
+soma = a + b
+print(soma)
+
+# ENTRADA DE DADOS
+nome = input('Insira o seu nome: ')
+idade = int(input('Insira a sua idade: '))
+print(f'Seu nome é {nome} e a sua idade é {idade}')
